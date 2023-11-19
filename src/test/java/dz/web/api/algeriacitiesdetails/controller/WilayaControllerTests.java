064c5a1484a6af5e6e577b53716986e5db45dc28
Messaoud GUERNOUTI <messaoud06@gmail.com>
2023-11-19 22:06:27 +0100

    add springdoc dependency

--- src/test/java/dz/web/api/algeriacitiesdetails/controller/WilayaControllerTests.java
+++ src/test/java/dz/web/api/algeriacitiesdetails/controller/WilayaControllerTests.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashSet;
@@ -27,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class WilayaControllerTests {
 
 
